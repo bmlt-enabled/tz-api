57176fb992065ef398217bb04cbda9f501cf6b80
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2020-10-19 21:37:38 -0400

    add domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,6 +158,60 @@ resource "aws_lambda_permission" "apigw" {
   source_arn = "${aws_api_gateway_rest_api.tz_api.execution_arn}/*/*"
 }
 
+data "aws_route53_zone" "tz" {
+  name = "tz.bmltenabled.org."
+}
+
+resource "aws_acm_certificate" "tz" {
+  domain_name       = "api.tz.bmltenabled.org"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "tz_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.tz.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.tz.zone_id
+}
+
+resource "aws_acm_certificate_validation" "tz" {
+  certificate_arn         = aws_acm_certificate.tz.arn
+  validation_record_fqdns = [for record in aws_route53_record.tz_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "tz" {
+  name    = aws_api_gateway_domain_name.tz.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.tz.id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.tz.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.tz.cloudfront_zone_id
+  }
+}
+
+resource "aws_api_gateway_domain_name" "tz" {
+  certificate_arn = aws_acm_certificate_validation.tz.certificate_arn
+  domain_name     = aws_acm_certificate.tz.domain_name
+}
+
+resource "aws_api_gateway_base_path_mapping" "tz" {
+  api_id      = aws_api_gateway_rest_api.tz_api.id
+  stage_name  = aws_api_gateway_deployment.example.stage_name
+  domain_name = aws_api_gateway_domain_name.tz.domain_name
+}
+
 output "base_url" {
   value = aws_api_gateway_deployment.example.invoke_url
 }
